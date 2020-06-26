cb83208d2eba67df38eb4dcf1857cd2102dfed41
marcoantonioap <65511251+marcoantonioap@users.noreply.github.com>
2020-06-26 17:43:57 -0300

    Starter SignUp Screen

--- src/pages/auth/signUp/index.js
+++ src/pages/auth/signUp/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    Image,
+    ScrollView,
+} from 'react-native';
+
+import logoImg from '../../../assets/mealclub_logo.png';
+import Icon from 'react-native-vector-icons/dist/Feather';
 
 import styles from './styles';
+import { TouchableOpacity, TextInput } from 'react-native-gesture-handler';
 
 export default function SignUp(){
     return(
-        <View style={styles.container}>
-            <Text>Sign Up Screen</Text>
-        </View>
+
+        <ScrollView style={styles.scrollContainer}>
+            <View style={styles.container}>           
+            
+                <View style={styles.headerContainer}>
+                    <Image source={logoImg} style={styles.logoimg}></Image>
+                    <Text style={styles.txtDescription}>MealClub</Text>
+                    <Text>faça parte desta família</Text>
+                </View>
+
+                <View style={styles.contentContainer}>
+                    
+                    <TextInput style={styles.allTxtInput} placeholderTextColor="#000" placeholder="Name" />                    
+                    <TextInput style={styles.allTxtInput} placeholderTextColor="#000" placeholder="Last Name" />
+
+                    <Icon name={'arrow-down'} size={20}/> 
+                    <TextInput style={[styles.allTxtInput, {width: '25%'}]} placeholderTextColor="#000" placeholder="UF" />
+                </View>
+
+            </View>
+        </ScrollView>
+       
     );
 }
