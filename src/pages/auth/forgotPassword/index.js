6c77a76ea7c3009ee69714280ba063df8d1e1d68
marcoantonioap <65511251+marcoantonioap@users.noreply.github.com>
2020-06-29 14:35:46 -0300

    Index Da ForgotPassword

--- src/pages/auth/forgotPassword/index.js
+++ src/pages/auth/forgotPassword/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
 
 import styles from './styles';
+import logoImg from '../../../assets/mealclub_logo.png';
+
+import Icon from 'react-native-vector-icons/dist/Feather';
 
 export default function ForgotPassword(){
     return(
+
         <View style={styles.container}>
-            <Text>Forgot Password Screen</Text>
+            
+            <Image source={logoImg} style={styles.logoimg} resizeMode="contain"></Image>    
+            
+            <View>
+                <Text style={styles.txtTitle}>Recuperação de senha</Text>
+                <Text>preencha o campo abaixo e deixe o resto com a gente</Text>
+            </View>
+            
+            
+            <View style={styles.containerEditPassword}>                            
+                <Icon size={25} name={"mail"} color="#e0201f"/>
+                    <TextInput style={styles.txtInputEmail} 
+                    placeholderTextColor="#b8ac95" 
+                    placeholder="Email/ Username"
+                    autoCapitalize={'none'}
+                    autoCorrect={false}
+                    keyboardType={'email-address'}
+                    blurOnSubmit={false}
+                    />
+            </View>
+            
+            <View style={styles.containerSubmitEdit}> 
+                
+                <TouchableOpacity style={styles.btnEditPassword}>
+                    <Text style={styles.txtNewPassword}>NOVA SENHA</Text>
+                    <Icon size={25} name={"edit"} color="#fff"></Icon>
+                </TouchableOpacity>
+            </View>
+            
         </View>
+       
     );
 }
