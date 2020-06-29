09e3082591d58991fb31312c9a79b5b899df04b2
marcoantonioap <65511251+marcoantonioap@users.noreply.github.com>
2020-06-29 14:36:22 -0300

    Styles da ForgotPassword

--- src/pages/auth/forgotPassword/styles.js
+++ src/pages/auth/forgotPassword/styles.js
@@ -7,5 +7,51 @@ export default StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        position: 'relative',
     },
+    logoimg:{
+        height:150,
+        position: 'relative',
+        marginTop: 25
+    },
+    containerEditPassword:{      
+        flexDirection: 'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        marginTop: 100,
+        marginBottom: 5,
+        position: 'relative'
+    },
+    containerSubmitEdit:{
+        flexDirection: 'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        position: 'relative',
+        marginBottom: 25
+    },
+    btnEditPassword:{
+        backgroundColor: '#e0201f',
+        height: 60,
+        alignItems: 'center',  
+        justifyContent: 'center',
+        borderRadius: 50,
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '50%',
+    },
+
+    //childrens
+    txtNewPassword:{
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    txtTitle:{
+        textAlign: 'center',
+        fontSize:25,
+        color:'#e0201f',
+        marginTop: 5,
+    }
+      
 });
