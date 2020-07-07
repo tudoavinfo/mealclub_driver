1c45b0abd46421a3346f8a1d5698400f64738b3b
marcoantonioap <65511251+marcoantonioap@users.noreply.github.com>
2020-07-07 16:57:14 -0300

    Tela de Lucros com Modal

--- src/pages/earnings/index.js
+++ src/pages/earnings/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import {View, Text, Image} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, Image, Modal} from 'react-native';
 import { Header, Icon } from 'react-native-elements';
 
-import { TouchableOpacity} from 'react-native-gesture-handler';
+import { TouchableOpacity, ScrollView} from 'react-native-gesture-handler';
+
+import styles from './styles';
 
 
 export default function Earnings(){
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalOpen2, setModalOpen2] = useState(false);
     return(
         <>
             <Header 
@@ -19,6 +23,201 @@ export default function Earnings(){
                 }}
                 
             />
+                <Modal visible={modalOpen} animated='slide'>  
+                    
+                        <View style={styles.modal1Container}>
+                                <TouchableOpacity>
+                                <Icon onPress={() => setModalOpen(false)} name={'x-circle'} type={'feather'} size={40} color={'#e02140'}/>
+                            </TouchableOpacity>
+
+                            <View style={{marginTop:30, marginBottom:30}}>
+                                <Text style={styles.personalNameModalTitle}>Total de indicados</Text>                        
+                            </View>
+                            
+                            <ScrollView>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Essa semana</Text>
+                                    <Text style={styles.txtModal}>4</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>  
+                                    <Text style={styles.personalNameModal}>Esse mês</Text>
+                                    <Text style={styles.txtModal}>10</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Último mês</Text>
+                                    <Text style={styles.txtModal}>12</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={[styles.personalNameModal,{fontWeight:'bold'}]}>Total</Text>
+                                    <Text style={styles.txtModal}>26</Text>
+                                </View>
+                            </ScrollView> 
+                            <TouchableOpacity style={styles.btnModal}>
+                                <Text style={styles.personalNameBtn} onPress={() => setModalOpen2(false)}>Ver Minha Rede</Text>
+                                <Icon name={'arrow-right'} type={'feather'} size={30} color='#fff' onPress={() => setModalOpen2(false)}/>
+                            </TouchableOpacity>                           
+                        </View>
+                                                                   
+                </Modal>
+
+                <Modal visible={modalOpen2} animated='slide'>   
+                    <View style={styles.modal1Container}>
+                            <TouchableOpacity>
+                            <Icon onPress={() => setModalOpen2(false)} name={'x-circle'} type={'feather'} size={40} color={'#e02140'}/>
+                        </TouchableOpacity>
+
+                        <View style={{marginTop:30, marginBottom:30}}>
+                            <Text style={styles.personalNameModalTitle}>Total de indicados</Text>                        
+                        </View>
+
+                            <View style={styles.defaultViewModal}>
+                                <Text style={[styles.txtModal1, {fontSize: 20, color:'#808080', opacity:1}]}>Nível</Text>
+                                <Text style={[styles.txtModal1, {fontSize: 20, color:'#808080', opacity:1}]}>Total</Text>
+                            </View>
+
+                            <ScrollView>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 1</Text>
+                                    <Text style={styles.txtModal}>2</Text>
+                                </View>
+
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 2</Text>
+                                    <Text style={styles.txtModal}>4</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 3</Text>
+                                    <Text style={styles.txtModal}>10</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 4</Text>
+                                    <Text style={styles.txtModal}>12</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 1</Text>
+                                    <Text style={styles.txtModal}>2</Text>
+                                </View>
+
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 2</Text>
+                                    <Text style={styles.txtModal}>4</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 3</Text>
+                                    <Text style={styles.txtModal}>10</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 4</Text>
+                                    <Text style={styles.txtModal}>12</Text>
+                                </View> 
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 1</Text>
+                                    <Text style={styles.txtModal}>2</Text>
+                                </View>
+
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 2</Text>
+                                    <Text style={styles.txtModal}>4</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 3</Text>
+                                    <Text style={styles.txtModal}>10</Text>
+                                </View>
+                                <View style={styles.defaultViewModal}>
+                                    <Text style={styles.personalNameModal}>Nível 4</Text>
+                                    <Text style={styles.txtModal}>12</Text>
+                                </View>                                                             
+                        </ScrollView>   
+                        <View style={styles.defaultViewModal}>
+                            <Text style={[styles.personalNameModal,{fontWeight:'bold'}]}>Total</Text>
+                            <Text style={styles.txtModal}>26</Text>
+                        </View>
+
+                        
+                            <TouchableOpacity style={styles.btnModal}>
+                                <Text style={styles.personalNameBtn} onPress={() => setModalOpen2(false)}>Ver Minha Rede</Text>
+                                <Icon name={'arrow-right'} type={'feather'} size={30} color='#fff' onPress={() => setModalOpen2(false)}/>
+                            </TouchableOpacity>    
+                             
+                    </View>                           
+                </Modal>
+            
+                <View style={styles.mapContainer}>
+                    <View style={styles.listContainer}>
+                        <ScrollView>
+                        <View style={styles.frstContainer}>
+                            <Text>http://localhost:3333/exemplolinkdeindicacao</Text>
+                            <View style={{right:70}}>
+                                <TouchableOpacity>
+                                    <Icon name={'copy'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>                                
+                            </View>
+                        </View>
+
+                        <View style={styles.defaultView}>
+                            <View  style={styles.scndContainer}>
+                                <Text>Consumo Mensal</Text>
+                                <TouchableOpacity>
+                                    <Icon name={'shopping-cart'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>
+                            </View>
+                            <View  style={styles.scndContainer}>
+                                <Text>Previsão de Lucros</Text>
+                                <TouchableOpacity>
+                                    <Icon name={'dollar-sign'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+
+                        <View style={styles.defaultView}>
+                            <View  style={styles.scndContainer}>
+                                <Text>Indicados diretos</Text>
+                                <TouchableOpacity>
+                                    <Icon name={'user-plus'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>
+                            </View>
+                            <View  style={styles.scndContainer}>
+                                <Text>Indicados indiretos</Text>
+                                <TouchableOpacity>
+                                    <Icon name={'user-plus'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>                                
+                            </View>
+                        </View>
+
+                        <View style={styles.defaultView}>
+                            <View  style={styles.scndContainer}>
+                                <Text>Patrocinador</Text>
+                                <TouchableOpacity>
+                                    <Icon name={'pie-chart'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>
+                            </View>
+                            <View  style={styles.scndContainer}>
+                                <Text>Plano</Text>
+                                <TouchableOpacity>
+                                    <Icon name={'pie-chart'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>                                
+                            </View>
+                        </View>
+
+                        <View style={styles.defaultView}>
+                            <View  style={styles.scndContainer}>
+                                <Text>Indicados Diretos</Text>
+                                <TouchableOpacity  onPress={() => setModalOpen(true)}>
+                                    <Icon name={'users'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>
+                            </View>
+                            <View  style={styles.scndContainer}>
+                                <Text>Indicados Indiretos</Text>
+                                <TouchableOpacity onPress={() => setModalOpen2(true)}>
+                                    <Icon name={'users'} type={'feather'} size={40} color={'#666'}/>
+                                </TouchableOpacity>                                
+                            </View>
+                        </View>                                               
+
+                        </ScrollView>                        
+                    </View>
+                </View>
+            
         </>
     );
 }
