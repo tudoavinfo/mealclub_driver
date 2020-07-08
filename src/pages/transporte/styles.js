098afb983084bb989bab7ded46cf489c83f4a183
marcoantonioap <65511251+marcoantonioap@users.noreply.github.com>
2020-07-08 17:55:29 -0300

    Update styles.js

--- src/pages/transporte/styles.js
+++ src/pages/transporte/styles.js
@@ -84,4 +84,146 @@ export default StyleSheet.create({
         alignItems:'center',
         padding:20,
     },
+
+
+    //modal
+    defaultView:{
+        flexDirection:'row', justifyContent:'space-between', marginBottom:15
+    },
+    personalNameModal:{
+        fontFamily:Platform.OS === 'android' ? 'Roboto' : 'Arial',
+        fontSize:15,
+        textAlign:'justify',
+        marginBottom:10,
+    },
+    txtModal:{
+        fontSize:15, 
+        color:'blue',
+        fontWeight:'bold',
+        opacity: 0.5
+    },
+    modal1Container:{
+        flex:1, 
+        paddingVertical:20, 
+        paddingHorizontal:20,
+        backgroundColor:'#f1f1f1'
+    },
+    defaultViewModal:{
+        flexDirection:'row', 
+        justifyContent:'space-between', 
+        marginBottom: 16,
+    },
+    personalNameModalTitle:{
+        fontFamily:Platform.OS === 'android' ? 'Roboto' : 'Arial',
+        fontSize:15,
+        textAlign:'justify',
+        marginBottom:10,
+        alignSelf:'center', 
+        fontSize: 20
+    },
+    personalNameBtn:{
+        fontFamily:Platform.OS === 'android' ? 'Roboto' : 'Arial',
+        fontSize:15,
+        textAlign:'justify',
+        marginBottom:10,
+        alignSelf:'center', 
+        fontSize: 20,
+        color:'#fff'
+    },
+    btnModal:{
+        backgroundColor: '#e02140',
+        padding:20,flexDirection:'row', 
+        justifyContent:'center', 
+        borderRadius: 10, 
+        borderWidth: 1.5, 
+        borderStyle:'dashed', 
+        borderColor:'#fff',
+        alignItems:'center', 
+        marginTop:30
+    },
+
+
+
+
+    titlePictureModal:{
+        alignSelf:'center',
+        alignItems:'center',
+        justifyContent:'center', 
+        borderRadius: 400/2, 
+        borderWidth:1, 
+        borderStyle:'dashed', 
+        borderColor:'#808080',
+        width: 100, 
+        height: 100, 
+        marginBottom:30
+    },
+    modalFrstPicture:{
+        width:'100%', 
+        backgroundColor:'#000', 
+        padding:80, 
+        paddingBottom:150, 
+        backgroundColor: '#f7f0d6', 
+        opacity:0.2, 
+        borderWidth:1, 
+        borderStyle:'dashed', 
+        borderRadius: 1, 
+        borderColor:'#000'
+    },
+    modalIconFrstPicture:{
+        alignSelf:'center', 
+        alignItems:'center', 
+        justifyContent:'center', 
+        borderRadius: 400/2, 
+        borderWidth:1, 
+        borderStyle:'dashed', 
+        borderColor:'#808080',
+        width: 120, 
+        height: 120, 
+        marginBottom:30
+    },
+    defaultgroupIcons:{
+        flexDirection:'row', 
+        justifyContent:'space-between', 
+        marginTop:2
+    },
+    defaulTxtModal:{
+        marginTop:16
+    },
+    transportModal:{
+        flexDirection:'row', 
+        opacity:0.8, 
+        alignItems:'center', 
+        justifyContent:'space-between', 
+        width:'100%', 
+        padding:5, 
+        height:80, 
+        borderWidth:1, 
+        borderStyle:'dashed',
+        borderRadius: 1,
+        borderColor:'#000', 
+        marginTop:2
+    },
+    numberAndFuel:{
+        flexDirection:'row', 
+        justifyContent:'space-between', 
+        marginTop:16
+    },
+    groupIconsTwo:{
+        flexDirection:'row', 
+        justifyContent:'space-between'
+    },
+    miniContainerDefault:{
+        flexDirection:'row', 
+        opacity:0.8, 
+        alignItems:'center', 
+        justifyContent:'space-between', 
+        padding:10, 
+        height:80, 
+        borderWidth:1, 
+        borderStyle:'dashed', 
+        borderRadius: 1, 
+        borderColor:'#000', 
+        marginTop:2
+    }
 });
+
